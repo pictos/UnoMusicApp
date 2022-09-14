834cc7cf9947a19bd76a9a4d7714f54d5fc69061
Pedro Jesus <pedrojesus.cefet@gmail.com>
2022-09-14 12:41:51 -0300

    SkiaSharp try

---
+++ UnoMusicApp/UnoMusicApp/CircleProgress.cs
@@ -0,0 +1,14 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnoMusicApp;
+internal class CircleProgress : SKCanvas
+{
+	public CircleProgress(SKBitmap bitmap) : base(bitmap)
+	{
+	}
+}
